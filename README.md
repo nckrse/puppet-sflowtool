8e85595f2dc1e24ee18dc13df852094a0c74d660
reese <reese@localhost.localdomain>
2014-12-11 13:57:52 -0600

    created initial files

---
+++ README.md
@@ -0,0 +1,1 @@
+README blah blah
---
+++ manifests/init.pp
@@ -0,0 +1,33 @@
+class sflowtool {
+case $::osfamily {
+'Debian': {
+file { '/tmp/sflowtool-3.22_amd64.deb':
+ensure => file,
+source => 'puppet:///modules/sflowtool/sflowtool-3.22_amd64.deb',
+owner => root,
+group => root,
+}->
+package { 'sflowtool-3.22_amd64':
+ensure => installed,
+provider => dpkg,
+source => '/tmp/sflowtool-3.22_amd64.deb',
+}
+}
+'RedHat': {
+file { '/tmp/sflowtool-3.22_x86_64.rpm':
+ensure => file,
+source => 'puppet:///modules/sflowtool/sflowtool-3.22_x86_64.rpm',
+owner => root,
+group => root,
+}->
+package { 'sflowtool-3.22_x86_64':
+ensure => installed,
+provider => rpm,
+source => '/tmp/sflowtool-3.22_x86_64.rpm',
+}
+}
+default: {
+fail('The module does not support this OS.')
+}
+}
+}
